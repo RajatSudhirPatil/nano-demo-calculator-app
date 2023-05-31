1767c2d52e33c9a03b5e29746222123ee6c54f83
RajatSudhirPatil <rajatsudhirpatil@gmail.com>
2023-06-01 00:19:07 +0530

    my commit

--- server.js
+++ server.js
@@ -10,15 +10,17 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('Hello World');
 });
 
 baseRouter.post('/add', (req, res) => {
+    res.json({"result":req.body.first+req.body.second});
     res.json({ "": null });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
+    res.json({"result":req.body.first+req.body.second});
     res.json({ "": null });
 });
 
